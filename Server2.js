7cff99e2cec1674ee31a6d83022d268d7e693928
Shifty <michiel.meurs1@gmail.com>
2019-11-26 14:29:51 +0100

    Added some features

--- Server2.js
+++ Server2.js
@@ -1,4 +1,14 @@
 const https = require('https');
+const http = require('http')
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+let displaydata='';
+
+
+
+
 
 https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
   let data = '';
@@ -13,8 +23,21 @@ https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
     console.log(JSON.parse(data).title);
     console.log();
     console.log(JSON.parse(data).explanation);
+    displaydata = data;
   });
 
 }).on("error", (err) => {
   console.log("Error: " + err.message);
 });
+
+const server = http.createServer(function(req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write(JSON.parse(displaydata).explanation);
+    res.end('Hello World\n');
+  });
+
+  server.listen(port, hostname, function() {
+    console.log('Server running at http://'+ hostname + ':' + port + '/');
+    console.log(displaydata)
+  });
