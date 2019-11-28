ad13abcbb882e8d0c93d5653efe851ac31b11f44
Shifty <michiel.meurs1@gmail.com>
2019-11-28 10:09:07 +0100

    First challenge complete FIX

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ const app = exp();
 const parse = require('body-parser');
 app.use(parse.json());
 
-var items = new Array(26).fill(0);
+var items = new Array(52).fill(0);
+let chars = ["a", "b", "c","d","e","f", "g", "h","i","j","k", "l", "m","n","o","p", "q", "r","s","t","u", "v", "w","x","y","z"
+, "A", "B","C","D","E", "F", "G","H","I","J", "K", "L","M","N", "O","P", "Q", "R","S","T","U", "V", "W","X","Y","Z"]
 
 function alphabetPosition(text) {
-  var result = "";
-  for (var i = 0; i < text.length; i++) {
-    var code = text.toUpperCase().charCodeAt(i)
-    if (code > 64 && code < 91) result += (code - 64) + " ";
+  for (var i = 0; i < chars.length; i++) {
+    if(chars[i] === text){
+      return i;
+    }
   }
-
-  return result.slice(0, result.length - 1);
+  return -1;
 }
 
 function get(char){
